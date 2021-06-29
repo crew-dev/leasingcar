945a13e445080abafa511f775aaada05e922a7f8
Vlad Dedikov <69351396+crew-dev@users.noreply.github.com>
2021-06-29 17:19:17 +0300

    upd//
    Политика конфиденциальности

--- js/script.js
+++ js/script.js
@@ -1,1 +1,138 @@
-let menuBurger=document.querySelector(".menu__burger"),menuClose=document.querySelector(".menu__close");function menuClick(){menuBurger.classList.toggle("active"),menuClose.classList.toggle("active")}menuBurger.addEventListener("click",menuClick,!1),menuClose.addEventListener("click",menuClick,!1);let navLink=document.querySelectorAll(".menu__link");navLink.forEach(function(e){e.addEventListener("click",menuClick,!1)});var elem=$("body > header"),doc=$(document);function scrolled(){var e=100<doc.scrollTop();elem.toggleClass("scrolled",e)}$(window).on({scroll:scrolled});let acc=document.getElementsByClassName("qa__current"),i;for(i=0;i<acc.length;i++)acc[i].addEventListener("click",function(){this.classList.toggle("active");var e=this.nextElementSibling;e.style.maxHeight?e.style.maxHeight=null:e.style.maxHeight=e.scrollHeight+"px"});let btnForm=document.querySelectorAll(".form__button, .popup__close"),openForm=document.querySelector(".popup"),formClick=function(){openForm&&openForm.classList.toggle("active")};btnForm.forEach(function(e){e.addEventListener("click",formClick,!1)});let btnModal=document.querySelectorAll(".modal__button, .modal__close"),openModal=document.querySelector(".modal"),modalClick=function(){openModal&&openModal.classList.toggle("active")};function validateForms(e,t){$(e).each(function(){$(this).validate({rules:{name:"required",tel:"required"},messages:{name:"Пожалуйста, введите ваше Имя",tel:"Пожалуйста, введите ваш номер телефона"},submitHandler:t})})}btnModal.forEach(function(e){e.addEventListener("click",modalClick,!1)}),window.addEventListener("DOMContentLoaded",function(){[].forEach.call(document.querySelectorAll(".tel"),function(e){var i;function t(e){e.keyCode&&(i=e.keyCode),this.selectionStart<3&&e.preventDefault();var t="+7 (___) ___ ____",n=0,l=t.replace(/\D/g,""),o=this.value.replace(/\D/g,""),c=t.replace(/[_\d]/g,function(e){return n<o.length?o.charAt(n++)||l.charAt(n):e});-1!=(n=c.indexOf("_"))&&(n<5&&(n=3),c=c.slice(0,n));t=t.substr(0,this.value.length).replace(/_+/g,function(e){return"\\d{1,"+e.length+"}"}).replace(/[+()]/g,"\\$&");(!(t=new RegExp("^"+t+"$")).test(this.value)||this.value.length<5||47<i&&i<58)&&(this.value=c),"blur"==e.type&&this.value.length<5&&(this.value="")}e.addEventListener("input",t,!1),e.addEventListener("focus",t,!1),e.addEventListener("blur",t,!1),e.addEventListener("keydown",t,!1)})}),validateForms("form",function(t){$.ajax({type:"POST",url:"/mailer/smart.php",data:$(t).serialize()}).done(function(e){$(t).find("input").val(""),alert(e.message),$(t).trigger("reset")})});
+/* === меню === */ 
+let menuBurger = document.querySelector('.menu__burger'),
+    menuClose = document.querySelector('.menu__close');
+
+function menuClick() {
+    menuBurger.classList.toggle("active");
+    menuClose.classList.toggle("active");
+}
+
+menuBurger.addEventListener('click', menuClick, false);
+menuClose.addEventListener('click', menuClick, false);
+
+/* === навигация, чтобы с меню переходило по ссылкам === */
+let navLink = document.querySelectorAll('.menu__link');
+
+navLink.forEach(function(node) {
+    node.addEventListener('click', menuClick, false);
+});
+
+/* === изменение при скролле === */
+var elem = $('body > header');
+var doc = $(document);
+function scrolled() {
+   var threshold = doc.scrollTop() > 100;
+   elem.toggleClass('scrolled', threshold);
+   }
+$(window).on({ scroll: scrolled });;
+let acc = document.getElementsByClassName("qa__current");
+let i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+      this.classList.toggle("active");
+      var panel = this.nextElementSibling;
+      if (panel.style.maxHeight) {
+        panel.style.maxHeight = null;
+      } else {
+        panel.style.maxHeight = panel.scrollHeight + "px";
+      }
+    });
+};
+// форма
+let btnForm = document.querySelectorAll('.form__button, .popup__close'),
+    openForm = document.querySelector('.popup');
+
+let formClick = function() {
+    openForm && openForm.classList.toggle('active')
+};
+
+btnForm.forEach(function(node) {
+    node.addEventListener('click', formClick, false)
+});
+
+// политика
+let btnModal = document.querySelectorAll('.modal__button, .modal__close'),
+    openModal = document.querySelector('.modal');
+
+let modalClick = function() {
+    openModal && openModal.classList.toggle('active')
+};
+
+btnModal.forEach(function(node) {
+    node.addEventListener('click', modalClick, false)
+});
+;
+/* ========== валидация номера телефона ========== */
+window.addEventListener("DOMContentLoaded", function() {
+    [].forEach.call( document.querySelectorAll('.tel'), function(input) {
+    var keyCode;
+    function mask(event) {
+        event.keyCode && (keyCode = event.keyCode);
+        var pos = this.selectionStart;
+        if (pos < 3) event.preventDefault();
+        var matrix = "+7 (___) ___ ____",
+            i = 0,
+            def = matrix.replace(/\D/g, ""),
+            val = this.value.replace(/\D/g, ""),
+            new_value = matrix.replace(/[_\d]/g, function(a) {
+                return i < val.length ? val.charAt(i++) || def.charAt(i) : a
+            });
+        i = new_value.indexOf("_");
+        if (i != -1) {
+            i < 5 && (i = 3);
+            new_value = new_value.slice(0, i)
+        }
+        var reg = matrix.substr(0, this.value.length).replace(/_+/g,
+            function(a) {
+                return "\\d{1," + a.length + "}"
+            }).replace(/[+()]/g, "\\$&");
+        reg = new RegExp("^" + reg + "$");
+        if (!reg.test(this.value) || this.value.length < 5 || keyCode > 47 && keyCode < 58) this.value = new_value;
+        if (event.type == "blur" && this.value.length < 5)  this.value = ""
+    }
+
+    input.addEventListener("input", mask, false);
+    input.addEventListener("focus", mask, false);
+    input.addEventListener("blur", mask, false);
+    input.addEventListener("keydown", mask, false)
+
+  });
+
+});
+
+/* ========== валидация ========== */
+function validateForms(form, handle) {
+    $(form).each(function(){$(this).validate({
+            rules: {
+                name: "required",
+                tel: "required",
+                // checkbox: "required"
+            },
+            messages: {
+                name: "Пожалуйста, введите ваше Имя",
+                tel: "Пожалуйста, введите ваш номер телефона",
+                // checkbox: ""
+            },
+            submitHandler: handle
+        });
+    });
+}
+validateForms('form', function(form) {
+    $.ajax({
+        // отдаем данные серверу
+        type: 'POST',
+        // какой обработчик будет обрабатывать операцию
+        url: '/mailer/smart.php',
+        // данные которые отправляем на сервер
+        data: $(form).serialize()
+    }).done(function(response) {
+        // чистим данные после отправки
+        $(form).find('input').val("");
+        alert(response.message);
+        // обновняем форму
+        $(form).trigger('reset');
+    });
+});
+;
+
